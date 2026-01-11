4f988c79651944f00aea8fcb9d586ecdce91b87e
hasnainahmedabie <hasnainahmedabir@gmail.com>
2026-01-11 12:34:03 +0600

    2nd

--- lap.js
+++ lap.js
@@ -8,3 +8,15 @@ function leapYea(year){
 }
 const tipi = leapYea(2028);
 console.log(tipi);
+//  leap year in different code
+function isLeapYear2(year){
+    if (year % 100 !==0 && year %4 === 0){
+        return true;
+    }
+    else if (year %100 ===0 && year %400 ===0){
+        return true;
+    }
+    else {return false};
+}4
+const lipi = isLeapYear2(2024);
+console.log(lipi);
